2dc4414d2792b3cf1694a958074a138a6e72f8f3
Harshit Srivastava <harshit100043@gmail.com>
2025-12-25 18:19:47 +0530

    agent created

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
-console.log("Hello via ]!");
+import { ChatGroq } from "@langchain/groq";
+import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
+import readline from "node:readline/promises";
+
+/***
+ * 1. Define the node function
+ * 2. Define the graph
+ * 3.Compile and invoked the graph
+ *
+ */
+
+/**
+ * Initialise the LLM
+ */
+const llm = new ChatGroq({
+  model: "openai/gpt-oss-120b",
+  temperature: 0,
+  timeout: null,
+  max_retries: 2,
+  //other params...
+});
+
+const callModel = async (state) => {
+  //call the LLM using API
+  console.log(`calling LLM...`);
+  const response = await llm.invoke(state.messages);
+  return { messages: [response] };
+};
+
+/**
+ * Build the graph
+ */
+
+const workflow = new StateGraph(MessagesAnnotation)
+  .addNode("agent", callModel)
+  .addEdge("__start__", "agent")
+  .addEdge("agent", "__end__");
+
+/**
+ * Compile the graph
+ */
+
+export const model = workflow.compile();
+
+const main = async () => {
+  try {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    while (true) {
+      const userInput = await rl.question("You: ");
+      if (
+        userInput === "/bye" ||
+        userInput === "/stop" ||
+        userInput === "/exit"
+      )
+        break;
+      const initialState = {
+        messages: [{ role: "user", content: userInput }],
+      };
+      const finalState = await model.invoke(initialState);
+
+      console.log("AI:", finalState.messages.at(-1).content);// to get last message we use -1
+    }
+    rl.close();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+main();
