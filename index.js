a467e58c37e68f5de26415256a80cfc4e3ab9561
Harshit Srivastava <harshit100043@gmail.com>
2025-12-25 21:39:16 +0530

    Backend for AI Agent Completed

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 import { ChatGroq } from "@langchain/groq";
 import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
 import readline from "node:readline/promises";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+
+import { TavilySearch } from "@langchain/tavily";
+import { MemorySaver } from "@langchain/langgraph";
+
+
+//const checkpointer = new SqliteSaver("memory.db");  //persistent 
+
+const checkpointer = new MemorySaver();
+
+
+const tool = new TavilySearch({
+    max_results:5,
+    topic:"general",
+    // include_answer:false,
+    // include_raw_content:false,
+    // include_images:false,
+    // include_image_descriptions:false,
+    // search_depth:"basic",
+    // time_range:"day",
+    // include_domains:null,
+    // exclude_domains:null
+})
+
+/**
+ * Initialise the tool node
+ */
+
+const tools = [tool];
+const toolNode = new ToolNode(tools);
+
 
 /***
  * 1. Define the node function
@@ -18,14 +49,26 @@ const llm = new ChatGroq({
   timeout: null,
   max_retries: 2,
   //other params...
-});
+}).bindTools(tools);
 
 const callModel = async (state) => {
   //call the LLM using API
   console.log(`calling LLM...`);
   const response = await llm.invoke(state.messages);
-  return { messages: [response] };
+  return { messages: [...state.messages,response] };
 };
+
+
+function shouldContinue(state){
+    //apply condition whether to call tool searching or not
+    const lastMessage = state.messages.at(-1);
+    console.log('state',state);
+    if(lastMessage.tool_calls && lastMessage.tool_calls.length > 0){
+        return "tools";
+    }
+    return '__end__'
+}
+
 
 /**
  * Build the graph
@@ -33,14 +76,17 @@ const callModel = async (state) => {
 
 const workflow = new StateGraph(MessagesAnnotation)
   .addNode("agent", callModel)
+  .addNode("tools",toolNode)
   .addEdge("__start__", "agent")
+  .addConditionalEdges("agent",shouldContinue)
+  .addEdge("tools","agent")
   .addEdge("agent", "__end__");
 
 /**
  * Compile the graph
  */
 
-export const model = workflow.compile();
+export const model = workflow.compile({checkpointer});
 
 const main = async () => {
   try {
@@ -59,7 +105,7 @@ const main = async () => {
       const initialState = {
         messages: [{ role: "user", content: userInput }],
       };
-      const finalState = await model.invoke(initialState);
+      const finalState = await model.invoke(initialState,{configurable:{thread_id: "1" }  });
 
       console.log("AI:", finalState.messages.at(-1).content);// to get last message we use -1
     }
